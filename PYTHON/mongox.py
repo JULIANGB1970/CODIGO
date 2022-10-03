1c5bad984cff10915df31b842e64a08a3a0c9693
JULIAN <jgbhub@gmail.com>
2022-10-03 17:07:02 +0200

    SUBO EL ARCHIVO MONGOX

---
+++ PYTHON/mongox.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from pymongo import MongoClient
+import json
+
+
+
+
+#Aquí creamos la conexión con una base de datos Mongo local; la instalación de mongo es gratuita, hay que tener un directorio /data/db/ colgando de C:>
+#y ejecutar los procesos mongod y mongos de la carpeta bin de la instalación de mongo en archivos de programa
+myclient =MongoClient("mongodb://localhost:27017/")
+
+
+#aquí leemos los datos de un archivo csv en un dataframe
+df = pd.read_csv("\DATA\VIH_ATTENDER_DIST.csv")
+
+
+#con esta instrucción genero un diccionario
+diccionario = df.to_dict("records")
+
+
+#creo una base de datos nueva en mongo
+mibase = myclient["vih"]
+
+#creo una tabala en la base de datos nueva
+mitabla = mibase["datos"]
+
+#inserto el diccionario tal cual en mongo, tal cual.
+mitabla.insert_many(diccionario)
+
+
+
+
+
+
+
+
+
+
+
+
